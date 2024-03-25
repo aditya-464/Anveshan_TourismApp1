0a207f259a86d869346d0393a6282e0010476542
Aditya Giri <adityagiri1911@gmail.com>
2024-03-25 12:53:41 +0530

    Assets added

---
+++ src/screens/Login.jsx
@@ -0,0 +1,96 @@
+import {
+  Dimensions,
+  Image,
+  ImageBackground,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React from 'react';
+import {COLORS, FONTFAMILY, FONTSIZE, SPACING} from '../theme/Theme';
+import LinearGradient from 'react-native-linear-gradient';
+
+const Login = () => {
+  return (
+    <SafeAreaView style={{flex: 1, width: '100%', height: '100%'}}>
+      <ImageBackground
+        style={styles.ImageBackground}
+        source={require('../assets/images/ok3.jpg')}>
+        <LinearGradient
+          colors={['rgba(0, 0, 0, 0)', 'rgba(0, 0, 0, 0.7)']}
+          style={styles.LinearGradient}>
+          <View style={styles.Content}>
+            <Text style={styles.Heading}>Welcome!</Text>
+            <Text style={styles.Subheading}>
+              Embark on a Journey of Discovery
+            </Text>
+            <TouchableOpacity activeOpacity={0.8} style={styles.Button}>
+              <Image
+                style={styles.Icon}
+                source={require('../assets/images/google.png')}></Image>
+              <Text style={styles.ButtonText}>Continue with Google</Text>
+            </TouchableOpacity>
+            <TouchableOpacity activeOpacity={0.8} style={styles.Button}>
+              <Image
+                style={styles.Icon}
+                source={require('../assets/images/apple.png')}></Image>
+              <Text style={styles.ButtonText}>Continue with Apple</Text>
+            </TouchableOpacity>
+          </View>
+        </LinearGradient>
+      </ImageBackground>
+    </SafeAreaView>
+  );
+};
+
+export default Login;
+
+const styles = StyleSheet.create({
+  ImageBackground: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'flex-end',
+  },
+  LinearGradient: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  Content: {
+    marginHorizontal: SPACING.space_24,
+    marginBottom: SPACING.space_30,
+  },
+  Heading: {
+    fontFamily: FONTFAMILY.cormorantGaramond_bold,
+    fontSize: 40,
+    color: COLORS.white,
+    textAlign: 'center',
+  },
+  Subheading: {
+    fontFamily: FONTFAMILY.harmattan_regular,
+    fontSize: FONTSIZE.size_20,
+    color: COLORS.white,
+    textAlign: 'center',
+    marginBottom: SPACING.space_30,
+  },
+  Button: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: SPACING.space_20,
+    backgroundColor: COLORS.white,
+    paddingVertical: SPACING.space_8,
+    borderRadius: 50,
+  },
+  Icon: {
+    width: 25,
+    height: 25,
+  },
+  ButtonText: {
+    fontFamily: FONTFAMILY.harmattan_medium,
+    fontSize: FONTSIZE.size_18,
+    color: COLORS.blackDark,
+    paddingLeft: SPACING.space_10,
+  },
+});
---
+++ src/theme/Theme.jsx
@@ -0,0 +1,73 @@
+export const SPACING = {
+  space_2: 2,
+  space_4: 4,
+  space_8: 8,
+  space_10: 10,
+  space_12: 12,
+  space_15: 15,
+  space_16: 16,
+  space_18: 18,
+  space_20: 20,
+  space_24: 24,
+  space_28: 28,
+  space_30: 30,
+  space_32: 32,
+  space_36: 36,
+  space_40: 40,
+};
+
+export const FONTSIZE = {
+  size_8: 8,
+  size_10: 10,
+  size_12: 12,
+  size_14: 14,
+  size_16: 16,
+  size_18: 18,
+  size_20: 20,
+  size_24: 24,
+  size_28: 28,
+  size_30: 30,
+  size_32: 32,
+  size_36: 36,
+  size_40: 40,
+};
+
+export const BORDERRADIUS = {
+  radius_4: 4,
+  radius_8: 8,
+  radius_10: 10,
+  radius_15: 15,
+  radius_20: 20,
+  radius_25: 25,
+};
+
+export const COLORS = {
+  red: '#dc3535',
+  orange: '#d17842',
+  blackDark: '#0c0f14',
+  blackLight: '#252a32',
+  greyDark: '#52555a',
+  greyLight: '#aeaeae',
+  white: '#ffffff',
+};
+
+export const FONTFAMILY = {
+  cormorantGaramond_regular: 'CormorantGaramond-Regular',
+  cormorantGaramond_medium: 'CormorantGaramond-Medium',
+  cormorantGaramond_semibold: 'CormorantGaramond-SemiBold',
+  cormorantGaramond_bold: 'CormorantGaramond-Bold',
+  cormorantGaramond_light: 'CormorantGaramond-Light',
+
+  cormorantInfant_regular: 'CormorantInfant-Regular',
+  cormorantInfant_medium: 'CormorantInfant-Medium',
+  cormorantInfant_semibold: 'CormorantInfant-SemiBold',
+  cormorantInfant_bold: 'CormorantInfant-Bold',
+  cormorantInfant_light: 'CormorantInfant-Light',
+
+  harmattan_regular: 'Harmattan-Regular',
+  harmattan_medium: 'Harmattan-Medium',
+  harmattan_semibold: 'Harmattan-SemiBold',
+  harmattan_bold: 'Harmattan-Bold',
+
+  lustria_regular: 'Lustria-Regular',
+};
